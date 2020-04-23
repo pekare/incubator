b33b5b46ffe35cc147c412179903d6867cdc4fd6
Carl Larsson <carl@pekare.se>
2020-04-23 20:32:42 +0200

    Update kali-setup.sh

--- kali-setup.sh
+++ kali-setup.sh
@@ -5,3 +5,9 @@ echo 'deb [arch=amd64] https://download.docker.com/linux/debian buster stable' |
 sudo apt update
 sudo apt install -y docker-ce
 sudo systemctl start docker
+
+
+# run gobuster
+ENDPOINT=http://10.10.180.182:3333
+WORDLIST=directory-list-2.3-medium.txt
+docker run -it --rm -v /usr/share/wordlists/dirbuster:/wordlists trickest/gobuster -u $ENDPOINT -w /wordlists/$WORDLIST
